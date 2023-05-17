851086311352251c1012fda432a2ec43d3ddeff2
Norbert László <laszlo.norbi2000@gmail.com>
2023-05-17 00:29:31 +0000

    deploy

--- develop/js/new-link.js
+++ develop/js/new-link.js
@@ -1,27 +1,42 @@
-import * as api from "./api.js"
+import * as api from "../../TrelloPowerUpNewMeeting/js/api.js"
 
 var Promise = TrelloPowerUp.Promise;
-const key = "2905a45608f989a24bf26e3d92edcf80"
+const key = "6f2af19073479657e48933387208eecd"
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 $(document).ready(function(){
+    var context = t.getContext();
+    var _boards;
+    t.getRestApi()
+    .getToken()
+    .then(token => {
+        if (!token) {
+            console.log("No token")
+        }
+        else{
+            getBoardsFromMember(context.member, key, token)
+            .then(boards => {
+                _boards = boards;
+                var element = $('#boardSelectorDropdown');
+                for (const board in boards){
+                    var opt = board.name;  
+                    var li = document.createElement("li");
+                    var link = document.createElement("a");             
+                    var text = document.createTextNode(opt);
+                    link.appendChild(text);
+                    link.href = "#";
+                    li.appendChild(link);
+                    element.appendChild(li);
+                }
+            })
+        }
+    })
     $('#meetingForm').submit(function(event){
         event.preventDefault();   
-        var listName =  $("#meetingstart")[0].value  + "-" + $("#meetingend")[0].value ;
-        console.log(listName);
-        const regex = /(\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d)-(\d\d):(\d\d)/i;
-        const match = listName.match(regex);
-        var starthour = parseInt(match[4]);
-        var endhour = parseInt(match[6]);
-        listName = $("#meetingName")[0].value + " " + `${match[1]}/${match[2]}/${match[3]}`;
-        var t = window.TrelloPowerUp.iframe({
-            appKey: '2905a45608f989a24bf26e3d92edcf80',
-            appName: 'Test'
-        });
-        var context = t.getContext();
+        var linkname = $("#linkName")[0].value;
         t.getRestApi()
         .getToken()
         .then(token => {
@@ -29,13 +44,13 @@ $(document).ready(function(){
                 console.log("No token")
             }
             else{
-                api.addList(listName, context.board, key, token)
+                api.addList(linkname, context.board, key, token)
                 .then(response => {
                     return response.text();
                 })
                 .then(text => {
                     const id = text.match(/"id":"([\da-z]*)"/i)[1]
-                    api.addCard("Summary","This is an automatically generated card.",id,key,token)
+                    api.addCard(linkname,"This is an automatically generated card.",id,key,token)
                     .then(response => {
                         return response.text();
                     })
@@ -44,12 +59,8 @@ $(document).ready(function(){
                         sleep(200) //The Api is slow and i need to wait otherwise i get no card with this id error.
                         .then(() => {
                             const id = cardJson.match(/"id":"([\da-z]*)"/i)[1];
-                            t.set(id, 'shared', 'meetingCard', {
-                                role: "Summary",
-                                startHour: starthour,
-                                startMinutes: parseInt(match[5]),
-                                endHour: endhour,
-                                endMinutes: parseInt(match[7])
+                            t.set(id, 'shared', 'onewaylink', {
+                                linktype: "list"
                             })
                             .then(idk => {
                                 t.closeModal();
