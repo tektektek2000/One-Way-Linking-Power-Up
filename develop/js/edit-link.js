26151cdd47fb2b609372f298cbb19d0153a4d670
Norbert László <laszlo.norbi2000@gmail.com>
2023-05-23 03:42:49 +0000

    deploy

--- develop/js/edit-link.js
+++ develop/js/edit-link.js
@@ -67,59 +67,54 @@ function saveCurrent(t){
             console.log("No token")
         }
         else{
-            promise = new Promise((resolve, reject) => {
-                resolve(JSON.parse($("#targetListSelectorDropdown")[0].value).id);
-            });
-            promise.then(id => {            
-                api.addCard(linkname,"This is an automatically generated card.",id,api.key,token)
-                .then(response => {
-                    return response.text();
+            var id = JSON.parse($("#targetListSelectorDropdown")[0].value).id          
+            api.addCard(linkname,"This is an automatically generated card.",id,api.key,token)
+            .then(response => {
+                return response.text();
+            })
+            .then(text => {
+                return sleep(200) //The Api is slow and i need to wait otherwise i get no card with this id error.
+                .then(() => {
+                    const id = text.match(/"id":"([\da-z]*)"/i)[1];
+                    return id;
                 })
-                .then(text => {
-                    return sleep(200) //The Api is slow and i need to wait otherwise i get no card with this id error.
-                    .then(() => {
-                        const id = text.match(/"id":"([\da-z]*)"/i)[1];
-                        return id;
-                    })
+            })
+            .then(id => { 
+                var type = "list";
+                var _linkTarget = _lists[$('#listSelectorDropdown')[0].value];
+                if($('#targetSelectorDropdown')[0].value === "Board"){
+                    type = "board";
+                    _linkTarget = $('#boardSelectorDropdown')[0].value;
+                }
+                var _condtype = "none";
+                var _condTarget = "";
+                if($('#conditionSelectorDropdown')[0].value === "Member"){
+                    _condtype = "member";
+                    _condTarget = _members[$('#memberConditionSelectorDropdown')[0].value];
+                }
+                else if($('#conditionSelectorDropdown')[0].value === "Label"){
+                    _condtype = "label";
+                    _condTarget = _labels[$('#labelConditionSelectorDropdown')[0].value];
+                }
+                t.set(id, 'shared', 'link', {
+                    linktype: type,
+                    linkTarget: _linkTarget,
+                    condtype: _condtype,
+                    condTarget: _condTarget,
+                    targetID: id
                 })
-                .then(id => { 
-                    var type = "list";
-                    var _linkTarget = _lists[$('#listSelectorDropdown')[0].value];
-                    if($('#targetSelectorDropdown')[0].value === "Board"){
-                        type = "board";
-                        _linkTarget = $('#boardSelectorDropdown')[0].value;
-                    }
-                    var _condtype = "none";
-                    var _condTarget = "";
-                    if($('#conditionSelectorDropdown')[0].value === "Member"){
-                        _condtype = "member";
-                        _condTarget = _members[$('#memberConditionSelectorDropdown')[0].value];
-                    }
-                    else if($('#conditionSelectorDropdown')[0].value === "Label"){
-                        _condtype = "label";
-                        _condTarget = _labels[$('#labelConditionSelectorDropdown')[0].value];
-                    }
-                    t.set(id, 'shared', 'link', {
-                        linktype: type,
-                        linkTarget: _linkTarget,
-                        condtype: _condtype,
-                        condTarget: _condTarget,
-                        targetID: id
-                    })
-                    .then(idk => {
-                        t.closeModal();
-                    })
-                    .catch(err => {
-                        console.error(err)
-                        t.closeModal();
-                    });
+                .then(idk => {
+                    t.closeModal();
                 })
                 .catch(err => {
                     console.error(err)
                     t.closeModal();
                 });
             })
-            .catch(err => console.error(err));
+            .catch(err => {
+                console.error(err)
+                t.closeModal();
+            });
         }
     });
 }
