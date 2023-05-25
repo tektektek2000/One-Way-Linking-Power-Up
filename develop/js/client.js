779b157d2c307acfbe1bfd54dd04995e97adf34f
Norbert László <laszlo.norbi2000@gmail.com>
2023-05-25 07:21:03 +0000

    deploy

--- develop/js/client.js
+++ develop/js/client.js
@@ -30,26 +30,88 @@ function showEditLinkMenu(t) {
     });
 }
 
+function saveChangesToCard(changed, newState, token){
+    api.updateCard(changed.id, newState.name, newState.desc, changed.idList, newState.closed, newState.idMembers, newState.idAttachmentCover, newState.due
+        ,newState.start, newState.dueComplete, newState.address, newState.locationName, newState.coordinates, newState.cover, api.key, token)
+}
+
 function modifyWithNewActions(modified, modifyWith, lastAcceptedValue){
     var stateChanged = false;
+    var checklistStateChanged = false;
     var newAcceptedValue = JSON.parse(JSON.stringify(modified));
     if(lastAcceptedValue.name !== modifyWith.name){newAcceptedValue.name = modifyWith.name; stateChanged = true;}
     if(lastAcceptedValue.desc !== modifyWith.desc){newAcceptedValue.desc = modifyWith.desc; stateChanged = true;}
     if(lastAcceptedValue.closed !== modifyWith.closed){newAcceptedValue.closed = modifyWith.closed; stateChanged = true;}
     //Checking if member was added
     for(let member of modifyWith.idMembers){
+        if(!lastAcceptedValue.idMembers.includes(member)){
+            stateChanged = true;
+        }
         if(!lastAcceptedValue.idMembers.includes(member) && !newAcceptedValue.idMembers.includes(member)){
             newAcceptedValue.idMembers.push(member);
         }
     }
     //Checking if member was removed
     for(let member of lastAcceptedValue.idMembers){
+        if(!modifyWith.idMembers.includes(member)){
+            stateChanged = true;
+        }
         if(!modifyWith.idMembers.includes(member) && newAcceptedValue.idMembers.includes(member)){
             newAcceptedValue.idMembers.splice(newAcceptedValue.idMembers.indexOf(member),1);
         }
     }
+    //Checking if checklist was added
+    while(lastAcceptedValue.checklists.length < modifyWith.checklists.length && newAcceptedValue.checklists.length < modifyWith.checklists.length){
+        newAcceptedValue.checklists.push(JSON.parse(JSON.stringify(modifyWith.checklists[modifyWith.checklists.length-1])));
+        checklistStateChanged = true;
+    }
+    //Checking if checklist was removed
+    while(lastAcceptedValue.checklists.length > modifyWith.checklists.length && newAcceptedValue.checklists.length > modifyWith.checklists.length){
+        newAcceptedValue.checklists.splice(newAcceptedValue.checklists.length-1,1);
+        checklistStateChanged = true;
+    }
+    for(var i in newAcceptedValue.checklists){
+        var examine = newAcceptedValue;
+        if(i < lastAcceptedValue.checklist.length){examine = lastAcceptedValue;}
+        if(examine.checklists[i].name !== modifyWith.checklists[i].name){newAcceptedValue.checklists[i].name = modifyWith.checklists[i].name; checklistStateChanged = true;}
+        //Checking if checklist was added
+        while(examine.checklists[i].checkItems.length < modifyWith.checklists[i].checkItems.length && newAcceptedValue.checklists[i].checkItems.length < modifyWith.checklists[i].checkItems.length){
+            newAcceptedValue.checklists[i].checkItems.push(JSON.parse(JSON.stringify(modifyWith.checklists[i].checkItems[modifyWith.checklists[i].checkItems.length-1])));
+            checklistStateChanged = true;
+        }
+        //Checking if checklist was removed
+        while(examine.checklists[i].checkItems.length > modifyWith.checklists[i].checkItems.length && newAcceptedValue.checklists[i].checkItems.length > modifyWith.checklists[i].checkItems.length){
+            newAcceptedValue.checklists[i].checkItems.splice(newAcceptedValue.checklists[i].checkItems.length-1,1);
+            checklistStateChanged = true;
+        }
+        for(var j in newAcceptedValue.checklists[i].checkItems){
+            var examine = newAcceptedValue;
+            if(j < lastAcceptedValue.checklist[i].checkItems.length){examine = lastAcceptedValue;}
+            if(examine.checklists[i].checkItems[j].name !== modifyWith.checklists[i].checkItems[j].name){
+                newAcceptedValue.checklists[i].checkItems[j].name = modifyWith.checklists[i].checkItems[j].name; checklistStateChanged = true;
+            }
+            if(examine.checklists[i].checkItems[j].state !== modifyWith.checklists[i].checkItems[j].state){
+                newAcceptedValue.checklists[i].checkItems[j].state = modifyWith.checklists[i].checkItems[j].state; checklistStateChanged = true;
+            }
+        }
+    }
     if(lastAcceptedValue.idAttachmentCover !== modifyWith.idAttachmentCover){newAcceptedValue.idAttachmentCover = modifyWith.idAttachmentCover; stateChanged = true;}
     //Lists are currently fucked, and will not be dealt with here
+    if(lastAcceptedValue.due !== modifyWith.due){newAcceptedValue.due = modifyWith.due; stateChanged = true;}
+    if(lastAcceptedValue.start !== modifyWith.start){newAcceptedValue.start = modifyWith.start; stateChanged = true;}
+    if(lastAcceptedValue.dueComplete !== modifyWith.dueComplete){newAcceptedValue.dueComplete = modifyWith.dueComplete; stateChanged = true;}
+    if(lastAcceptedValue.address !== modifyWith.address){newAcceptedValue.address = modifyWith.address; stateChanged = true;}
+    if(lastAcceptedValue.locationName !== modifyWith.locationName){newAcceptedValue.locationName = modifyWith.locationName; stateChanged = true;}
+    if(lastAcceptedValue.coordinates !== modifyWith.coordinates){newAcceptedValue.coordinates = modifyWith.coordinates; stateChanged = true;}
+    if(lastAcceptedValue.cover.color !== modifyWith.cover.color){newAcceptedValue.cover.color = modifyWith.cover.color; stateChanged = true;}
+    if(lastAcceptedValue.cover.brightness !== modifyWith.cover.brightness){newAcceptedValue.cover.brightness = modifyWith.cover.brightness; stateChanged = true;}
+    if(lastAcceptedValue.cover.url !== modifyWith.cover.url){newAcceptedValue.cover.url = modifyWith.cover.url; stateChanged = true;}
+    if(lastAcceptedValue.cover.idAttachment !== modifyWith.cover.idAttachment){newAcceptedValue.cover.idAttachment = modifyWith.cover.idAttachment; stateChanged = true;}
+    if(lastAcceptedValue.cover.size !== modifyWith.cover.size){newAcceptedValue.cover.size = modifyWith.cover.size; stateChanged = true;}
+    return {
+        state: newAcceptedValue,
+        changed: stateChanged
+    }
 }
 
 function syncChanges(t,links,token,linkedCard){
@@ -63,18 +125,15 @@ function syncChanges(t,links,token,linkedCard){
         first = linkedCard.card;
         second = linkedCard.originalCard;
     }
-    var newAcceptedState = modifyWithNewActions(linkedCard.link.lastAcceptedValue,first,linkedCard.link.lastAcceptedValue).state;
-    newAcceptedState = modifyWithNewActions(newAcceptedState,second,linkedCard.link.lastAcceptedValue).state;
-    var context = t.getContext();
-    api.getMembersFromBoard(context.board,api.key,token)
-    .then(values => {
-        var members = [];
-        for(let it of values){
-            members.push(it.id);
-        }
-        return members;
-    })
-
+    var newAcceptedState = modifyWithNewActions(linkedCard.link.lastAcceptedValue,first,linkedCard.link.lastAcceptedValue);
+    var firstHasChanges = newAcceptedState.changed;
+    newAcceptedState = modifyWithNewActions(newAcceptedState.state,second,linkedCard.link.lastAcceptedValue);
+    if(firstHasChanges){
+        saveChangesToCard(second, newAcceptedState.state)
+    }
+    else if(newAcceptedState.changed){
+        saveChangesToCard(first, newAcceptedState.state)
+    }
 }
 
 function refreshCards(t,links,token){
@@ -113,7 +172,9 @@ function refreshCards(t,links,token){
                     }
                 }
             }
-            console.log(linkedCards);
+            for(var it of linkedCards){
+                syncChanges(t,links,token,it);
+            }
         })
     })
 }
