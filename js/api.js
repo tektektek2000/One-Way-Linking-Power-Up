aaa313edd12038065aeef4638588dbf17944813f
Norbert László <laszlo.norbi2000@hotmail.com>
2023-05-25 02:30:22 +0200

    This should fix the API overuse now

--- js/api.js
+++ js/api.js
@@ -9,232 +9,220 @@ function addList(listName, boardID, apiKey, token){
 }
 
 function addCard(cardName, cardDesc, listID, apiKey, token){
-    return fetch(`https://api.trello.com/1/cards?idList=${listID}&name=${cardName}&desc=${cardDesc}&key=${apiKey}&token=${token}`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var card = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return card;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/cards?idList=${listID}&name=${cardName}&desc=${cardDesc}&key=${apiKey}&token=${token}`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var card = JSON.parse(text);
+            return card;
+        });
+    })
 }
 
 function copyCard(listID, copyFromId, apiKey, token){
-    return fetch(`https://api.trello.com/1/cards?idList=${listID}&idCardSource=${copyFromId}&key=${apiKey}&token=${token}`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var card = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return card;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/cards?idList=${listID}&idCardSource=${copyFromId}&key=${apiKey}&token=${token}`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var card = JSON.parse(text);
+            return card;
+        });
+    })
 }
 
 function getBoardsFromMember(memberID, apiKey, token){
-    return fetch(`https://api.trello.com/1/members/${memberID}/boards?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var boards = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return boards;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/members/${memberID}/boards?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var boards = JSON.parse(text);
+            return boards;
+        });
+    })
 }
 
 function getCardsFromBoard(boardID, apiKey, token){
-    return fetch(`https://api.trello.com/1/boards/${boardID}/cards?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var cards = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return cards;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/boards/${boardID}/cards?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var boards = JSON.parse(text);
+            return boards;
+        });
+    })
 }
 
 function getListsFromBoard(boardID, apiKey, token){
-    return fetch(`https://api.trello.com/1/boards/${boardID}/lists?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var lists = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return lists;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/boards/${boardID}/lists?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var boards = JSON.parse(text);
+            return boards;
+        });
+    })
 }
 
 function getMembersFromBoard(boardID, apiKey, token){
-    return fetch(`https://api.trello.com/1/boards/${boardID}/members?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var members = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return members;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/boards/${boardID}/members?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var boards = JSON.parse(text);
+            return boards;
+        });
+    })
 }
 
 function getLabelsFromBoard(boardID, apiKey, token){
-    return fetch(`https://api.trello.com/1/boards/${boardID}/labels?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var labels = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return labels;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/boards/${boardID}/labels?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var boards = JSON.parse(text);
+            return boards;
+        });
+    })
 }
 
 function getList(listID, apiKey, token){
-    return fetch(`https://api.trello.com/1/lists/${listID}?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var list = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return list;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/lists/${listID}?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var list = JSON.parse(text);
+            return list;
+        });
+    })
 }
 
 function getCard(cardID, apiKey, token){
-    return fetch(`https://api.trello.com/1/cards/${cardID}?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var card = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return card;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/cards/${cardID}?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var card = JSON.parse(text);
+            return card;
+        });
+    })
 }
 
 function getBoard(boardID, apiKey, token){
-    return fetch(`https://api.trello.com/1/boards/${boardID}?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var board = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return board;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/boards/${boardID}?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var board = JSON.parse(text);
+            return board;
+        });
+    })
 }
 
 
 function getCardsFromList(listID, apiKey, token){
-    return fetch(`https://api.trello.com/1/lists/${listID}/cards?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var cards = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return cards;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/lists/${listID}/cards?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var cards = JSON.parse(text);
+            return cards;
+        });
+    })
 }
 
 function getChecklist(checkListId, apiKey, token){
-    return fetch(`https://api.trello.com/1/checklists/${checkListId}?key=${apiKey}&token=${token}`, {
-        method: 'GET',
-        headers: {
-        'Accept': 'application/json'
-        }
-    })
-    .then(response => {
-        return response.text();
-    })
-    .then(text => {
-        var checklist = JSON.parse(text);
-        return sleep(800)
-        .then(() => {
-            return checklist;
+    return sleep(100).then(() => {
+        return fetch(`https://api.trello.com/1/checklists/${checkListId}?key=${apiKey}&token=${token}`, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json'
+            }
         })
-    });
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            var checklist = JSON.parse(text);
+            return checklist;
+        });
+    })
 }
 
 const key = "6f2af19073479657e48933387208eecd"
